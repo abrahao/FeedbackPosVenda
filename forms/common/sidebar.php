31074bd504548552a33a9973ef58b45f45d50a1f
abrahao <abrahao.eneias@dce.ufpb.br>
2022-02-14 10:16:23 -0300

    first commit

---
+++ forms/common/sidebar.php
@@ -0,0 +1,15 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item nav-profile">
+        </li>
+        <li class="nav-item nav-category">
+            <span class="nav-link">MENU</span>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/forms/relatorios.php">
+                <span class="menu-title">Relatórios</span>
+                <i class="icon-screen-desktop menu-icon"></i>
+            </a>
+        </li>
+    </ul>
+</nav>
